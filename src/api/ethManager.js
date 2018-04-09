7c2f5af119aa434183dee2b1df84342ead14a2c5
Sébastien de Beauffort <sewelseb@hotmail.com>
2018-04-09 16:13:34 +0200

    settings

--- src/api/ethManager.js
+++ src/api/ethManager.js
@@ -6,8 +6,13 @@ var Web3 = require('web3');
 var web3 = new Web3();
 
 
-//TODO: set the port i get from geth
+
+//GETH COMMAND TO LAUNCH ETH SERVER: geth --rpc --rpcaddr "127.0.0.1" --rpcport "8545" \
+// --rpc --rpccorsdomain "http://regis.nu" console
 web3.setProvider(new web3.providers.HttpProvider("http://localhost:8545"));
+console.log('web3 is connected:'+web3.isConnected());
+//web3.setProvider(new web3.providers.HttpProvider("http://localhost:30303"));
+//web3.setProvider(new web3.providers.HttpProvider("http://52.169.42.101:30303"));
 
 export default ({ config, db }) => {
     let api = Router();
@@ -18,7 +23,29 @@ export default ({ config, db }) => {
 
     api.get('/createWallet', (req, res) => {
 
-        //console.log(web3);
+        /*if (typeof web3 !== 'undefined') {
+            web3 = new Web3(web3.currentProvider);
+        } else {
+            // set the provider you want from Web3.providers
+            web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+        }*/
+
+
+        if(web3.isConnected())
+        {
+            console.log('web3 is connected');
+            var password= "test";
+
+            console.log(web3.eth.accounts);
+
+            var account  = web3.eth.accounts.create(password);
+            //web3.eth.accounts.create();
+            //console.log(account);
+        }
+        else {
+            res.json({ "ERROR": "web3 not connected" });
+        }
+
 
 
         res.json({ "TODO": "CREATE WALLET AND RETURN PRIVATE KEY AND PUBLIC ADDRESS" });

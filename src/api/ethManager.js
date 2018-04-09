7c98a10bab153d746def0ef81c6ea970337561c1
Sébastien de Beauffort <sewelseb@hotmail.com>
2018-04-09 17:00:38 +0200

    ip

--- src/api/ethManager.js
+++ src/api/ethManager.js
@@ -9,8 +9,8 @@ var web3 = new Web3();
 
 //GETH COMMAND TO LAUNCH ETH SERVER: geth --rpc --rpcaddr "127.0.0.1" --rpcport "8545" \
 // --rpc --rpccorsdomain "http://regis.nu" console
-web3.setProvider(new web3.providers.HttpProvider("http://localhost:8545"));
-console.log('web3 is connected:'+web3.isConnected());
+web3.setProvider(new web3.providers.HttpProvider("http://192.168.1.9:8545"));
+//console.log('web3 is connected:'+web3.isConnected());
 //web3.setProvider(new web3.providers.HttpProvider("http://localhost:30303"));
 //web3.setProvider(new web3.providers.HttpProvider("http://52.169.42.101:30303"));
 

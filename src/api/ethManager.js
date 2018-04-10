76a77c116eb084cefb1dd5a7d6e8b0631612d855
Sébastien de Beauffort <sewelseb@hotmail.com>
2018-04-10 12:16:27 +0200

    finalisation

--- src/api/ethManager.js
+++ src/api/ethManager.js
@@ -12,10 +12,7 @@ var web3 = new Web3("http://127.0.0.1:8545");
 
 //GETH COMMAND TO LAUNCH ETH SERVER: geth --rpc --rpcaddr "127.0.0.1" --rpcport "8545" \
 // --rpc --rpccorsdomain "http://regis.nu" console
-//web3.setProvider(new Web3.providers.HttpProvider("http://192.168.1.9:8545"));
-//console.log('web3 is connected:'+web3.isConnected());
-//web3.setProvider(new web3.providers.HttpProvider("http://localhost:30303"));
-//web3.setProvider(new web3.providers.HttpProvider("http://52.169.42.101:30303"));
+
 
 export default ({ config, db }) => {
     let api = Router();
@@ -26,23 +23,12 @@ export default ({ config, db }) => {
 
     api.get('/createWallet', (req, res) => {
 
-        /*if (typeof web3 !== 'undefined') {
-            web3 = new Web3(web3.currentProvider);
-        } else {
-            // set the provider you want from Web3.providers
-            web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-        }*/
-        //console.log(web3._requestManager);
-
-
-        //console.log('web3 is connected');
-        //var password= "test";
 
         console.log(web3.eth.accounts);
 
-        //var account  = web3.eth.accounts.create(password);
+
         var account  = web3.eth.accounts.create();
-        //web3.eth.accounts.create();
+        
         console.log(account);
 
 
